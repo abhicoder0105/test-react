900374287dd7efacf083ed720334cd9e5261d797
abhicoder0105 <sharmaabhishek9770@gmail.com>
2021-08-08 14:59:52 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Modal from './components/Modal';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="test">
+        <h1>
+         <Modal/>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
